57cd62c73507f941791731de4a9a14a2f1a67809
Adam Hair <Adam Hair>
2017-12-08 18:06:24 -0600

    Buttons work, but there's no back end yet. Close to testing file read/processing.

--- app/src/main/java/edu/tamu/adamhair/kaldroid/MainActivity.java
+++ app/src/main/java/edu/tamu/adamhair/kaldroid/MainActivity.java
@@ -3,6 +3,7 @@ package edu.tamu.adamhair.kaldroid;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -16,18 +17,33 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
 import java.util.HashMap;
+import java.nio.*;
 
 import edu.cmu.pocketsphinx.Assets;
 import edu.cmu.pocketsphinx.Hypothesis;
 import edu.cmu.pocketsphinx.RecognitionListener;
 import edu.cmu.pocketsphinx.SpeechRecognizer;
+import edu.cmu.pocketsphinx.Config;
+import edu.cmu.pocketsphinx.Decoder;
+import edu.cmu.pocketsphinx.Segment;
 import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
 
 import static android.widget.Toast.makeText;
+import static junit.framework.Assert.fail;
 
 public class MainActivity extends AppCompatActivity implements RecognitionListener {
 
+    private enum LanguageModel {
+        ENGLISH,
+        ENGLISH_DYSARTHRIC,
+        ENGLISH_CHILDREN,
+        ENGLISH_DYSARTHRIC_CHILDREN
+    };
 
     /* Named searches allow to quickly reconfigure the decoder */
     private static final String KWS_SEARCH = "wakeup";
@@ -49,6 +65,9 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
     Button enDysButton;
     Button enKidButton;
     Button enDysKidButton;
+    TextView infoLabel;
+    ProgressBar pBar;
+    boolean ignoreButtons = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +78,10 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
         enDysButton = (Button) findViewById(R.id.en_us_dys);
         enKidButton = (Button) findViewById(R.id.en_us_kid);
         enDysKidButton = (Button) findViewById(R.id.en_us_dys_kid);
+        infoLabel = (TextView) findViewById(R.id.infoLabel);
+        pBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        infoLabel.setText("Select a language model to process");
 
         // Check if user has given permission to record audio
         int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
@@ -71,21 +94,112 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
         enButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ProgressBar pBar = (ProgressBar) findViewById(R.id.progressBar);
-                pBar.setVisibility(View.VISIBLE);
-                pBar.animate();
+                handleButtonPress(LanguageModel.ENGLISH);
+            }
+        });
+
+        enDysButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonPress(LanguageModel.ENGLISH_DYSARTHRIC);
+            }
+        });
+
+        enKidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonPress(LanguageModel.ENGLISH_CHILDREN);
             }
         });
 
         enDysKidButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ProgressBar pBar = (ProgressBar) findViewById(R.id.progressBar);
-                pBar.setVisibility(View.VISIBLE);
-                pBar.animate();
-                
+                handleButtonPress(LanguageModel.ENGLISH_DYSARTHRIC_CHILDREN);
             }
         });
+    }
+
+    private void handleButtonPress(LanguageModel model) {
+        if (!ignoreButtons) {
+            // Don't take button input until one has finished
+            ignoreButtons = true;
+
+            switch(model) {
+                case ENGLISH:
+                    infoLabel.setText("Processing with English model");
+                    processAudio();
+                    break;
+                case ENGLISH_DYSARTHRIC:
+                    infoLabel.setText("Processing with English/Dysarthric model");
+                    processAudio();
+                    break;
+                case ENGLISH_CHILDREN:
+                    infoLabel.setText("Processing with English/Kid model");
+                    processAudio();
+                    break;
+                case ENGLISH_DYSARTHRIC_CHILDREN:
+                    infoLabel.setText("Processing with English/Dysarthric/Kid model");
+                    processAudio();
+                    break;
+                default:
+                    infoLabel.setText("Button did not work. Try again.");
+                    ignoreButtons = false;
+                    break;
+            }
+        }
+    }
+
+    private void processAudio() {
+        pBar.setVisibility(View.VISIBLE);
+
+        // Process audio with PocketSphinx and appropriate model
+
+
+        // Processing is done, accept input again
+        ignoreButtons = false;
+        pBar.setVisibility(View.INVISIBLE);
+        infoLabel.setText("Select a language model to process");
+    }
+
+    private void processAudioFile(String filename) {
+        Config c = Decoder.defaultConfig();
+        c.setString("-hmm", "../../model/en-us/en-us");
+        c.setString("-lm", "../../model/en-us/en-us.lm.dmp");
+        c.setString("-dict", "../../model/en-us/cmudict-en-us.dict");
+        Decoder d = new Decoder(c);
+        FileInputStream stream = null;
+
+        String filenameAndPath = Environment.getExternalStorageDirectory().getPath() + "/testAudio/" + filename;
+
+        try {
+            stream = new FileInputStream(new File(filenameAndPath));
+        } catch (Exception e) {
+            // Handle exception
+        }
+
+        d.startUtt();
+        byte[] b = new byte[4096];
+        try {
+            int nbytes;
+            while ((nbytes = stream.read(b)) >= 0) {
+                ByteBuffer bb = ByteBuffer.wrap(b, 0, nbytes);
+
+                // Not needed on desktop but required on android
+                bb.order(ByteOrder.LITTLE_ENDIAN);
+
+                short[] s = new short[nbytes/2];
+                bb.asShortBuffer().get(s);
+                d.processRaw(s, nbytes/2, false, false);
+            }
+        } catch (IOException e) {
+            fail("Error when reading goforward.wav" + e.getMessage());
+        }
+        d.endUtt();
+        System.out.println(d.hyp().getHypstr());
+        for (Segment seg : d.seg()) {
+            System.out.println(seg.getWord());
+        }
     }
 
     private void runRecognizerSetup() {
@@ -183,13 +297,13 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
                 .setAcousticModel(new File(assetsDir, "en-us-ptm"))
                 .setDictionary(new File(assetsDir, "cmudict-en-us.dict"))
 
-                .setRawLogDir(assetsDir) // To disable logging of raw audio comment out this call (takes a lot of space on the device)
+                //.setRawLogDir(assetsDir) // To disable logging of raw audio comment out this call (takes a lot of space on the device)
                 .setKeywordThreshold(1e-45f) // Threshold to tune for keyphrase to balance between false alarms and misses
                 .setBoolean("-allphone_ci", true)  // Use context-independent phonetic search, context-dependent is too slow for mobile
 
-
                 .getRecognizer();
         recognizer.addListener(this);
+
 
         /** In your application you might not need to add all those searches.
          * They are added here for demonstration. You can leave just one.
